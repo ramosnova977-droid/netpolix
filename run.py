7cf0e87f66bf79913bfd363860d37590be76a0d3
soficpz <cplauraso13@gmail.com>
2026-05-30 09:59:39 -0500

    feat: agregar panel admin y vista cliente mejorada

--- run.py
+++ run.py
@@ -14,10 +14,15 @@ app.register_blueprint(venta_bp)
 app.register_blueprint(calificacion_bp)
 app.register_blueprint(video_bp)
 app.register_blueprint(cliente_bp)
+app.register_blueprint(gerente_bp)
 
 @app.route("/")
 def home():
     return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "netpolix.html")
+
+@app.route("/admin")
+def admin():
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
