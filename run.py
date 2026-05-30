2cc7877a024faff70fbda12ad691b01a58a370c6
soficpz <cplauraso13@gmail.com>
2026-05-30 00:16:37 -0500

    actualizacion

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
 
 from app.routes.video_routes import video_bp
 from app.routes.cliente_routes import cliente_bp
-
 from app.routes.venta_routes import venta_bp
 from app.routes.calificacion_routes import calificacion_bp
 from app.routes.gerente_routes import gerente_bp
-    
+
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(venta_bp)
@@ -15,9 +15,9 @@ app.register_blueprint(calificacion_bp)
 app.register_blueprint(video_bp)
 app.register_blueprint(cliente_bp)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return {"message": "Backend funcionando"}
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "netpolix.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
